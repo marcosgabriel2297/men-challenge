8bc1ecdd78fa3b6f64844d315cb86fc7a17a92a3
Marcos Gabriel Vera <mvera@moodtechnology.com.ar>
2022-12-29 03:29:54 -0300

    created service to add comment to post

--- src/services/postService.js
+++ src/services/postService.js
@@ -4,17 +4,24 @@ const { Post } = models;
 
 const create = (post) => Post.create(post);
 
-const findAll = () => Post.find({});
+const findAll = () => Post.find({}, { comments: 0 });
 
 const findById = (id) => Post.findById(id);
 
-const findByAuthor = (authorId) => Post.find({ author: authorId });
+const findByAuthor = (authorId) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  Post.find({ author: authorId }, { comments: 0 });
+
+const addCommentToPost = (idPost, comment) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  Post.updateOne({ _id: idPost }, { $push: { comments: comment } });
 
 const postService = {
   create,
   findAll,
   findById,
   findByAuthor,
+  addCommentToPost,
 };
 
 export default postService;
