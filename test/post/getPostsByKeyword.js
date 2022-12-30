6dfb79be2de708da3b81c4e509a7b590ac8757b5
Marcos Gabriel Vera <mvera@moodtechnology.com.ar>
2022-12-30 03:34:41 -0300

    created test to get posts by keyword

--- test/post/getPostsByKeyword.js
+++ test/post/getPostsByKeyword.js
@@ -2,7 +2,6 @@ import chai from 'chai';
 import mocha from 'mocha';
 import '../../app';
 import axios from 'axios';
-import mongoose from 'mongoose';
 import User from '../../src/models/user';
 import Post from '../../src/models/post';
 import { signJwt } from '../../src/utils/jwtUtil';
@@ -18,9 +17,9 @@ const { before, after } = mocha;
 const { describe, it } = mocha;
 const { assert } = chai;
 
-const { USER_NOT_EXISTS } = errorCodes;
+const { POST_NOT_EXISTS } = errorCodes;
 
-let existingPost;
+// let existingPost;
 let existingUser;
 let existingUserToken;
 
@@ -29,7 +28,7 @@ const instance = axios.create({
   baseURL: BASE_URL,
 });
 
-const path = (postId) => `${POSTS}/author/${postId}`;
+const path = (keyword) => `${POSTS}/keyword/${keyword}`;
 
 describe('Post Controller', () => {
   before(async () => {
@@ -38,40 +37,54 @@ describe('Post Controller', () => {
     existingUserToken = signJwt(existingUser);
   });
 
-  describe(`GET ${POSTS}/author/:id`, () => {
+  describe(`GET ${POSTS}/keyword/:keyword`, () => {
     before(async () => {
-      existingPost = await generatePost({ author: existingUser._id });
+      await generatePost({
+        title: 'fakeTitle',
+        body: 'comentario positivo',
+      });
     });
 
     it('Should return unauthorized as no header is sent', async () => {
       try {
-        await instance.get(path(existingPost._id));
+        await instance.get(path('positivo'));
         assert.fail();
       } catch (err) {
         assert.equal(err.response.status, 401);
       }
     });
 
-    it('Should return bad request as author not found', async () => {
+    it('Should return bad request as keyword not found', async () => {
       try {
         await instance.get(
-          path(mongoose.Types.ObjectId()),
+          path('negativo'),
           buildAuthorizationHeader(existingUserToken),
         );
         assert.fail();
       } catch (err) {
-        assert.equal(err.response.status, 422);
-        assert.equal(err.response.data.errors[0].msg, USER_NOT_EXISTS);
+        assert.equal(err.response.status, 404);
+        assert.equal(err.response.data.message, POST_NOT_EXISTS);
       }
     });
 
-    it('Should return posts by author successfully', async () => {
-      const posts = await instance.get(
-        path(existingUser._id),
+    it('Should return posts filtered by "fakeTitle"', async () => {
+      const response = await instance.get(
+        path('fakeTitle'),
         buildAuthorizationHeader(existingUserToken),
       );
 
-      assert.equal(posts.status, 200);
+      assert.equal(response.status, 200);
+      assert.equal(response.data.length, 1);
+    });
+
+    it('Should return posts filtered by "positivo"', async () => {
+      const response = await instance.get(
+        path('positivo'),
+        buildAuthorizationHeader(existingUserToken),
+      );
+
+      assert.equal(response.status, 200);
+      assert.equal(response.data.length, 1);
     });
 
     after(async () => {
